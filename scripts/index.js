63991257eafbddd2c5eb582d05e13ae496b34cf8
MJaurigue <jaurigue.michael@gmail.com>
2022-11-26 19:22:46 -0600

    added links to index.js

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "../images/yosemite.jpg",
+  },
+
+  {
+    name: "Lake Louise",
+    link: "../images/lake-louise.jpg",
+  },
+
+  {
+    name: "Bald Mountains",
+    link: "../images/bald-mountains.jpg",
+  },
+
+  {
+    name: "Latemar",
+    link: "../images/latemar.jpg",
+  },
+
+  {
+    name: "Vanoise National Park",
+    link: "../images/vanoise.jpg",
+  },
+
+  {
+    name: "Lago di Braies",
+    link: "../images/lago.jpg",
+  },
+];
