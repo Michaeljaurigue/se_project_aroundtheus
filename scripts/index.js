2c9277f0467a694ce0742267bdf2336937cba693
MJaurigue <jaurigue.michael@gmail.com>
2022-12-30 11:08:14 -0600

    added location button and trash and like button usability

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,13 @@
-// Variables
+// Profile Pop Up Variables
 const editProfilePopUp = document.querySelector(".modal__edit");
-
-const editProfileButton = document.querySelector(".profile__edit");
 
 const editProfileForm = document.querySelector(".modal__form-edit");
 
 const saveProfileEditForm = document.querySelector(".modal__form-edit");
 
-const closeEditProfileButton = document.querySelector(
-  ".modal__container_close-button"
-);
+const closeEditProfileButton = document.querySelector(".modal__close-edit");
+
+const editProfileButton = document.querySelector(".profile__edit");
 
 const profileName = document.querySelector(".profile__name");
 
@@ -19,11 +17,16 @@ const nameInput = document.querySelector(`.form__input[name="name"]`);
 
 const jobInput = document.querySelector(`.form__input[name="description"]`);
 
-// Open Profile Modal
-
+//Modal Open and Close Functions
 function openModal(modal) {
   modal.classList.add("modal__opened");
 }
+
+function closeModal(modal) {
+  modal.classList.remove("modal__opened");
+}
+
+// Profile Pop Up Open Functions
 
 function openProfileModal() {
   const profileName = document.querySelector(".profile__name").textContent;
@@ -37,19 +40,14 @@ function openProfileModal() {
 
 editProfileButton.addEventListener("click", openProfileModal);
 
-// Close Profile Modal
-
-function closeModal(modal) {
-  modal.classList.remove("modal__opened");
-}
-
-closeEditProfileButton.addEventListener("click", closeProfileModal);
+// Profile Pop Up Close Functions
 
 function closeProfileModal() {
   closeModal(editProfilePopUp);
 }
 
-// Submit Button
+closeEditProfileButton.addEventListener("click", closeProfileModal);
+// Profile Pop Up Submit Button
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -120,5 +118,85 @@ function getCardElement(data) {
   cardImage.setAttribute("src", data.link);
   cardImage.setAttribute("alt", `Photo of ${data.name}`);
 
+  cardImage.addEventListener("click", (evt) => openImageModal(evt, data));
+
+  const heart = card.querySelector(".card__like-button");
+  heart.addEventListener("click", (evt) =>
+    evt.target.classList.toggle("card__like-button_active")
+  );
+
+  const trash = card.querySelector(".card__trash");
+  trash.addEventListener("click", (evt) =>
+    evt.target.closest(".card").remove()
+  );
+
   return card;
 }
+
+//Add a Location Pop Up Variables
+const addLocationPopUp = document.querySelector(".modal_add-location");
+
+const addLocationButton = document.querySelector(".profile__add-button");
+
+const addLocationForm = document.querySelector(".modal__form-new-location");
+
+//Add a Location Pop Up Functions
+
+const openLocationModal = () => {
+  openModal(addLocationPopUp);
+};
+
+addLocationButton.addEventListener("click", openLocationModal);
+
+//Close a Location Pop Up Close Functions
+
+function closeNewLocationModal() {
+  closeModal(addLocationPopUp);
+}
+
+const closeNewLocationButton = document.querySelector(
+  ".modal__close-new-location"
+);
+
+closeNewLocationButton.addEventListener("click", closeNewLocationModal);
+
+//Save Location Variables and Functions
+const saveLocationForm = document.querySelector(".modal_add-location");
+
+const saveNewLocation = (evt) => {
+  evt.preventDefault();
+  const titleInput = document.getElementById("title").value;
+  const imageUrlInput = document.getElementById("image-url").value;
+  const cardObject = { link: imageUrlInput, name: titleInput };
+  const card = getCardElement(cardObject);
+  cardsList.prepend(card);
+  addLocationForm.reset();
+  closeNewLocationModal();
+};
+
+saveLocationForm.addEventListener("submit", saveNewLocation);
+
+//Iamge Pop Up Variables
+const imageModal = document.querySelector(".modal__image");
+
+//Image Pop Up Open Functions
+
+const openImageModal = (evt, data) => {
+  imagePreview.src = evt.target.src;
+  imagePreview.alt = `Photo of ${data.name}`;
+  imageText.textContent = data.name;
+  openModal(imageModal);
+};
+
+//Image Pop Up Close Functions
+
+const closeImageModalButton = document.querySelector(".modal__image-close");
+
+const imagePreview = imageModal.querySelector(".modal__img");
+const imageText = imageModal.querySelector(".modal__image-text");
+
+function closeImageModal() {
+  closeModal(imageModal);
+}
+
+closeImageModalButton.addEventListener("click", closeImageModal);
